a0a5576e0ffcea3bfde3dad3dc7d63290eea055a
Lukas <lukasz.kodym@gmail.com>
2020-04-05 13:17:26 +0200

    frst init

---
+++ data_types.py
@@ -0,0 +1,4 @@
+a =1
+b = 4.5
+print(isinstance(b, float))
+print(isinstance(a, int))
---
+++ if.py
@@ -0,0 +1,32 @@
+# a = 1
+# if a == 0:
+#     print('to jest prawda')
+# else:
+#     print('to jest fałsz')
+# print('koniec programu')
+
+# name = input('Podaj swoje imię: ')
+# if len(name) < 3:
+#     print('podaj minimum 3 znaki')
+# else:
+#     print(name)
+
+# name = input('Podaj nazwę języka programowania: ')
+# if name == 'python':
+#     print('To jest python!')
+# elif name == 'php':
+#     print('To jest php!')
+# elif name == 'java':
+#     print('To jest java!')
+# else:
+#     print('Nie znam tego języka')
+
+# a = False
+# b = 4
+# c = 5
+# if not a: # and, or, not
+#     print('Warunek spełniony')
+
+a = False
+if a:
+    print('Warunek spełniony')
