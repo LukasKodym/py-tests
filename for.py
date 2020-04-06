6225a3fab2ce2e3085bbc2c964c418f1bf9861f3
Lukas <lukasz.kodym@gmail.com>
2020-04-06 10:50:36 +0200

    new commit

---
+++ for.py
@@ -0,0 +1,11 @@
+# values = [1,2,3,4,5]
+# for v in values:
+#     if v >=4:
+#         break
+#     print(v)
+
+# for i in range(1, 20, 3):
+#     print(i)
+
+for i in range(20):
+    print('*' * i)
---
+++ while.py
@@ -0,0 +1,21 @@
+# counter = 0
+# while counter < 10:
+#     counter +=1
+#     if counter == 5:
+#         break
+#     print(counter)
+
+# counter = 1
+# while counter <= 10:
+#     counter +=1
+#     if counter % 2 != 0:
+#         continue
+#     print(counter)
+
+while True:
+    num = int(input('Podaj liczbę większą od 0: '))
+    if num > 0:
+        print('Twoja liczba to:', num)
+        break
+    else:
+        print('Twoja liczba nie jest prawidłowa')
