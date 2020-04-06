b6a7058b89422c72c273f941b1774541cde8df4a
Lukas <lukasz.kodym@gmail.com>
2020-04-06 14:12:43 +0200

    new commit

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,32 @@
-options ={
+# options = {
+#     'env': 'production',
+#     'db': "mysql",
+#     'version': 1.0,
+#     'show_errors': True
+# }
+# options['user'] = "admin"
+# print(options)
+# options['version'] = 2.0
+# print(options['db'])
+# print(options['version'])
+
+options = {
     'env': 'production',
     'db': "mysql",
     'version': 1.0,
     'show_errors': True
 }
-options['user'] = "admin"
+print(options.get('version'))
+del options['db']
+options.update({
+    'user': 'admin',
+    'app': 0,
+    'version': 2.2
+})
 print(options)
-options['version'] = 2.0
-print(options['db'])
-print(options['version'])
+print(options.get('db'))
+print(options.get('version'))
+for key in options:
+    print(options[key])
+print(options.values())
+print(options.keys())
