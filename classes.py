5edb4b4e37292e2917510e32a2fd1833cdc6a3d9
Lukas <lukasz.kodym@gmail.com>
2020-04-09 10:23:05 +0200

    update

---
+++ classes.py
@@ -0,0 +1,42 @@
+# class Point:
+#     pass # place holder
+# p1 = Point()
+# p2 = Point()
+# print(p1)
+# print(p1, p2)
+# print(type(p1))
+# print(type(p2))
+
+# class Point:
+#     def __init__(self): # bez ustawiania parametrów na wejściu
+#         self.x = 0
+#         self.y = 0
+# p1 = Point() # powstaje punkt o współrzędnych domyślnych
+# p2 = Point()
+# print(p1.x, p1.y)
+# print(p2.x, p2.y)
+
+# class Point:
+#     def __init__(self, x = 0, y = 0): # można też ustawić wartości domyślne na wejściu
+#         self.x = x
+#         self.y = y
+# p1 = Point(2, 3)
+# p2 = Point()
+# print(p1.x, p1.y)
+# print(p2.x, p2.y)
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def move_to_new_coords(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+p1 = Point(2, 3)
+print(p1.x, p1.y)
+# # nowe przypisanie zmiennych, można zastąpić metodą w klasie
+# p1.x = 9
+# p1.y = 12
+# print(p1.x, p1.y)
+p1.move_to_new_coords(9, 12)
+print(p1.x, p1.y)
