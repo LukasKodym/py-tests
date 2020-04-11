e759706618e8ebadc9e8881e1bdda9186a8fa0ab
Lukas <lukasz.kodym@gmail.com>
2020-04-11 11:30:49 +0200

    name update

---
+++ spyder/input.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# %%
+imie = input('Podaj Twoje imie: ')
+
+# print('Czesc', imie)
+print('Czesc {}!'.format(imie))
+
+# %%
+jezyk = input('Jakiego języka chcesz się nauczyć?\n')
+
+print('Podano język {}!'.format(jezyk))
+
+# %%
+
+imie = input('Podaj imię: ')
+age = input('Podaj wiek: ')
+
+print('Czesc {}! Masz {} lat.'.format(imie, age))
+
+# %%
+age_2 = int(input('Podaj wiek: '))
