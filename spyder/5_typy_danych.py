08aeba33019918b2f81c605db4059496e472108e
Lukas <lukasz.kodym@gmail.com>
2020-04-14 12:36:15 +0200

    update

---
+++ spyder/5_typy_danych.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+string = 'Python'
+
+print(dir(string))
+
+# %%
+
+a = 10
+print(dir(a))
+
+
+# %%
+
+type(a)
+type(string)
+
+# %%
+
+b = 4.5
+type(b)
+
+# %%
+
+d = 3 + 3j
+print(dir(d))
+type(d)
+
+# %%
+
+type(True)
+type(False)
+
+value = True
+print(value)
+
+# %%
+
+x = '1323435'
+y = 12334
+z = '0'
+print('x: {}\ny: {}\nz: {}'.format(type(x), type(y), type(z)))
+# print('x:',type(x),'\ny:',type(y),'\nz:',type(z))
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
---
+++ spyder/6_string_metody.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+text = 'Witaj na kursie Pythona.\nPython jest super!'
+
+print(text)
+
+# %%
+
+dir(text)
+help(str.count)
+
+# %%
+text.capitalize()
+
+# %%
+text.title()
+
+# %%
+text.count('Python')
+
+# %%
+text.startswith('witaj')
+'python'.startswith('py')
+
+# %%
+text.endswith('r!')
+
+# %%
+'sample.py'.endswith('.py')
+
+# %%
+text.find('Python')
+text[text.find('Python'):]
+
+# %%
+hashtags = 'sport#gym'
+idx = hashtags.find('#')
+hashtags[:idx]
+
+# %%
+'pawel5555 '.isalnum()
+
+# $$
+'43434ljkjhk'.isdigit()
+
+# %%
+'pyThon'.islower()
+'PAwEL'.isupper()
+
+# %%
+' '.join(['python', '3,7'])
+'#'.join(['sport', 'gym', 'fit'])
+','.join(['1', '2', '3', '4'])
+
+# %%
+'#good#time#mood'.replace('#', ' ')
+'column name'.replace(' ', '_')
+
+# %%
+'    python       '.strip()
+'    python       '.rstrip()
+'    python       '.lstrip()
+
+# %%
+'1,2,3,4,5'.split(',')
+'java python php sql sas'.split()
+'#gym#fit#mood'.split('#')
+
+# %%
+'12'.zfill(5)
+'1'.zfill(10)
+
+# %%
+'#'.join(['sport','python','free','time'])
+
+x = '123,785,45,5'
+print(x.split(','))
+
+
+
+
+
+
+
+
+
+
+
+
+
