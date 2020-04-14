db5c15810b8cee5b5419a2a2062882f2c68d3c93
Lukas <lukasz.kodym@gmail.com>
2020-04-14 10:56:45 +0200

    update

---
+++ spyder/4_wycinanie.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+name = 'Python'
+
+# %%
+
+print(name)
+print(name[2])
+print(name[-2])
+print(name[-1])
+print(name[::-1])
+
+# %%
+# name[start:stop]
+# name[:stop]
+# name[start:]
+
+name[1:4]
+name[:4]
+name[2:]
+name[:]
+
+# %%
+
+name[-2:]
+name[-3:-1]
+
+# %%
+
+full = 'Python Programming'
+print(full[7:])
+print(full[::2])
+
+# %%
+ 
+sample = '#stop#this#flow'
+
+print(sample[::5])
+
+# %%
+
+numbers = '8,9,7,4'
+print(numbers[::2])
+
+# %%
+
+print(numbers[::-1])
+
+# %%
+name = 'kajak'
+print(name[::-1])
+
+# %%
+
+name = 'Python'
+
+'P' in name
+'p' in name
+
+
+
+
+
+
+
+
+
+
