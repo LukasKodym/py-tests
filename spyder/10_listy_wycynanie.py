94bd03432379bd84358a3aa450a5e71c180b19ac
Lukas <lukasz.kodym@gmail.com>
2020-04-16 14:31:51 +0200

    update

---
+++ spyder/10_listy_wycynanie.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+index = [ 0,  1,  2,  3,  4,  5]
+idx_n = [-6, -5, -4, -3, -2, -1]
+lista = [34, 23, 56, 24, 28, 76]
+
+# %%
+
+# lista[start:stop]
+# lista[index]
+# lista[:stop]
+# lista[start:]
+# lista[::step]
+
+lista[0]
+lista[1]
+lista[-1]
+lista[-3]
+lista[3]
+
+# %%
+lista[1:5]
+lista[-5:-1]
+lista[::-1]
+
+# %%
+lista[:4]
+lista[:-2]
+lista[3:]
---
+++ spyder/12_słownik.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+empty_dict = dict()
+print(empty_dict)
+
+d = {}
+print(type(d))
+
+# e = set()
+
+# %% metody tworzenia pustych struktur danych w Python
+
+a = ()
+b = []
+c = {}
+
+e = set()
+f = tuple()
+g = dict()
+h = list()
+
+# %%
+pol_to_eng = {'jeden':'one', 'dwa':'two', 'trzy':'three'}
+
+name_to_digit = {'jeden':1, 'dwa':2, 'trzy':3}
+
+# %%
+index_to_digit = {0:1, 1:2, 2:3}
+
+len(index_to_digit)
+
+# %%
+pol_to_eng['cztery'] = 'four'
+
+# %%
+pol_to_eng.clear()
+
+# %%
+pol_to_eng_copied = pol_to_eng.copy()
+
+# %%
+pol_to_eng.keys()
+# list(pol_to_eng.keys())
+
+# %%
+pol_to_eng.values()
+# list(pol_to_eng.values())
+
+# %%
+pol_to_eng.items()
+# list(pol_to_eng.items())
+
+# %%
+pol_to_eng['jeden']
+
+# %%
+pol_to_eng.get('jeden', 'NaN')
+pol_to_eng.get('zero', 'NaN')
+
+# %%
+pol_to_eng.pop('dwa')
+pol_to_eng.popitem()
+
+# %%
+pol_to_eng.update({'jeden':1})
