7c8906cf51be715b771f4d274ee4161b3492cf22
Lukas <lukasz.kodym@gmail.com>
2020-04-22 16:27:47 +0200

    update

--- loops.py
+++ loops.py
@@ -31,3 +31,37 @@ for i in range(1, 8):
 
 # %% zad 7
 num = [1, 2, 3]
+string = ['a', 'b', 'c']
+
+for i in num:
+    for j in string:
+        print(i, j)
+
+# %% zad 8
+a = list(range(1,9))
+print(a)
+
+# %% zad 9
+for x in a:
+    if x >= 5:
+        break
+    print(x)
+
+# %% zad 10
+x = 4
+while x < 19:
+    if x % 2 == 0:
+        print(x)
+    x += 1
+
+# %% zad 11
+tab = ['A', 'B', 'C', 'D']
+for i in len(tab):
+    i += 1
+    
+
+
+
+
+
+
