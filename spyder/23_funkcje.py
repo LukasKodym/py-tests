9a6886852a80778a2b4f4129220240b128ca2d9d
Lukas <lukasz.kodym@gmail.com>
2020-04-24 12:29:18 +0200

    update

--- spyder/23_funkcje.py
+++ spyder/23_funkcje.py
@@ -79,3 +79,47 @@ def print_menu():
 
 
 print_menu()
+
+
+# %%
+def print_even(maximum):
+    even = []
+    for i in range(maximum + 1):
+        if i % 2 == 0:
+            even.append(i)
+    return even
+
+
+# print_even(10)
+num = print_even(20)
+
+
+# %%
+def write_file(file_name, text):
+    with open(file_name, 'w') as file:
+        print(text, file=file)
+
+
+write_file('sample_3.txt', 'Pierwsza linia.\nDruga linia.')
+write_file('sample_4.txt', 'Pierwsza. \nDruga. \nTrzecia.')
+
+
+# %%
+def calculate_profit(pv=1000, rate=0.03, n=1):
+    return pv * (1 + rate) ** n - pv
+
+
+# calculate_profit(1000, 0.2, 15)
+calculate_profit()
+
+
+# %%
+def drukuj_nieparzyste(maksimum=20):
+    odd = []
+    for i in range(maksimum + 1):
+        if i % 2 != 0:
+            odd.append(i)
+    return odd
+
+
+drukuj_nieparzyste()
---
+++ spyder/24_docs.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+def add(a: int, b: int):
+    """
+    Zwraca sumę dwóch liczb.    
+
+    Parameters
+    ----------
+    a : TYPE
+        INTIGER.
+    b : TYPE
+        INTIGER.
+
+    Returns
+    -------
+    TYPE
+    SUM : TYPE
+        INTIGER.
+
+    """
+    return a + b
+
+
+# %%
+help(add)
