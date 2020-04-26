97d8beb94f4b3140c31c672b6681d17d3836610f
lukas <lukasz.kodym@gmail.com>
2020-04-26 22:23:31 +0200

    fibonacci example

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 22:18:29 2020
+
+@author: lukas
+"""
+
+
+def fib(num: int):
+    a, b = 0, 1
+    for i in range(num):
+        a, b = b, a + b
+    return a
+
+
+n = int(input('Który wyraz ciągu Fibonacciego policzyć? '))
+
+val = fib(n)
+
+print('{}. wyraz ciągu wynosi: {}'.format(n, val))
