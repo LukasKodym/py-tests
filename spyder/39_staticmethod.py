3c4ddf0ec5517907e8e012df0beacde51576e959
Lukas <lukasz.kodym@gmail.com>
2020-05-15 22:48:27 +0200

    oop update

--- spyder/39_staticmethod.py
+++ spyder/39_staticmethod.py
@@ -2,25 +2,27 @@
 
 # %%
 ##
+
+
 class Student:
-    
     lista_studentow = []
-    
+
     def __init__(self, imie, nazwisko, wiek):
         self.imie = imie
         self.nazwisko = nazwisko
         self.wiek = wiek
         self.lista_studentow.append(self)
-    
+
     @staticmethod
     def liczba_studentow():
         print('Liczba studentów:', len(Student.lista_studentow))
-        
+
+
 # %%
 ##
 student_1 = Student('Jan', 'Kowalski', 18)
 student_2 = Student('Tomasz', 'Nowak', 23)
-student_3 = Student('Jan','Nowak',20)
+student_3 = Student('Jan', 'Nowak', 20)
 
 # %%
 ##
--- spyder/41_projekt_magazyn.py
+++ spyder/41_projekt_magazyn.py
@@ -1,59 +1,59 @@
 # -*- coding: utf-8 -*-
 
+# import sys
+# sys.exit() 
+
+
 class Magazyn:
-    
+
     def __init__(self, lista_produktow):
         self.lista_produktow = lista_produktow
-        
-        
-    def wyswietl_dostepne_produkty(self):
-        print('Dostepne produkty:')
+
+    def wydruk_listy(self):
         for produkt in self.lista_produktow:
             print(produkt)
-            
+
+    def wyswietl_dostepne_produkty(self):
+        print('Dostepne produkty:\n')
+        self.wydruk_listy()
+
     def dodaj_produkt(self):
         self.nazwa_produktu = input('Podaj nazwę produktu: >>> ')
         if self.nazwa_produktu not in self.lista_produktow:
             self.lista_produktow.append(self.nazwa_produktu)
-        print(f'Produkt {self.nazwa_produktu} został dodany do magazynu')
-    
+        print(f'\nProdukt {self.nazwa_produktu} został dodany do magazynu.\n')
+        self.wydruk_listy()
+
     def usun_produkt(self):
-        self.nazwa_produktu = input('Podaj nazwę produktu, który chcesz'
+        self.nazwa_produktu = input('Podaj nazwę produktu, który chcesz '
                                     'usunąć: >>> ')
         if self.nazwa_produktu in self.lista_produktow:
             self.lista_produktow.remove(self.nazwa_produktu)
-            print('Usunięto produkt z magazynu.')
+            print('\nUsunięto produkt z magazynu.')
+            self.wydruk_listy()
         else:
-            print('Podanego produktu nie ma na magazynie.')
+            print('\nPodanego produktu nie ma na magazynie.')
+
 
 # %%
 ##
 magazyn = Magazyn(['mleko', 'woda', 'jajka'])
 
-print('Wprowadź 1 aby wyswietlić stan magazynu.')
-print('Wprowadź 2 aby dodać produkt.')
-print('Wprowadź 3 aby usunąć produkt.')
-print('Wprowadź 4 aby zakończyć')
-wybor_uzytkownia =  
+while True:
+    print('\nWprowadź 1 aby wyswietlić stan magazynu.')
+    print('Wprowadź 2 aby dodać produkt.')
+    print('Wprowadź 3 aby usunąć produkt.')
+    print('Wprowadź 4 aby zakończyć')
+    wybor_uzytkownia = input('>>> ')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if wybor_uzytkownia == str('1'):
+        magazyn.wyswietl_dostepne_produkty()
+    elif wybor_uzytkownia == str('2'):
+        magazyn.dodaj_produkt()
+    elif wybor_uzytkownia == str('3'):
+        magazyn.usun_produkt()
+    elif wybor_uzytkownia == str('4'):
+        print('\nKoniec programu...')
+        break
+    else:
+        print('\nBłędny wybór, spróbuj jeszcze raz.')
