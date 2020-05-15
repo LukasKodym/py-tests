65e0309918ce8954c03adfce59729629bc2a72d5
Lukas <lukasz.kodym@gmail.com>
2020-05-15 14:49:31 +0200

    oop in python

---
+++ spyder/37_parametr_self.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# %%
+##
+
+class Drzewo:
+    
+    def wyswietl_info_o_drzewie(self):
+#        pass
+        self.nazwa = 'Sosna'
+        self.wiek = 30
+        print(f'Drzewo {self.nazwa} ma {self.wiek}.')
+ 
+   
+drzewo = Drzewo()
+
+# %%
+##
+drzewo.wyswietl_info_o_drzewie()
+
+# %%
+##
+Drzewo.wyswietl_info_o_drzewie(drzewo)
