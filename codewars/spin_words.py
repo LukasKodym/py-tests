e9b700e8350a2d3c798410ded82b6c1f5840216c
Lukas <lukasz.kodym@gmail.com>
2020-07-06 20:56:00 +0200

    update

--- codewars/spin_words.py
+++ codewars/spin_words.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
+
 def spin_words(sentence):
     if len(sentence) == 0:
         return None
     words = sentence.split(" ")
-    for wordindex,word in enumerate(words):
-        if len(word)>=5:
+    for wordindex, word in enumerate(words):
+        if len(word) >= 5:
             words[wordindex] = reverse(word)
     return " ".join(word for word in words)
-          
-    
+
+
 def reverse(word):
-  l = len(word)
-  res = ""
-  for index in range(1, l+1):
-      res += word[-index]
-  return res
+    l = len(word)
+    res = ""
+    for index in range(1, l + 1):
+        res += word[-index]
+    return res
+
 
 """
 print(spin_words('Ten samochód należy do mnie'))
--- spyder/57_doc_test.py
+++ spyder/57_doc_test.py
@@ -29,3 +29,5 @@ if __name__ == '__main__':
     import doctest
 
     doctest.testmod()
+
+print('','','', sep='X')
